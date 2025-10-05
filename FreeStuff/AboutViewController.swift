2993e4d700e13c568cf31f2b347d07bbfcd95c59
NinaWie <wnina@student.ethz.ch>
2025-10-05 16:11:13 +0200

    feat: add more information to the description

--- FreeStuff/AboutViewController.swift
+++ FreeStuff/AboutViewController.swift
@@ -19,7 +19,7 @@ class AboutViewController: UIViewController {
         self.label.contentMode = .scaleToFill
         self.label.numberOfLines = 30
 
-        self.label.text = "Let's stop trashing perfectly good stuff! This app helps you to find free things around you, like furniture, clothes, or food.\n\nThis is Free Stuff v\(currentVersion ?? "").\n\n©Nina Wiedemann (2025)"
+        self.label.text = "Let's stop trashing perfectly good stuff! This app helps you to find free things around you, like furniture, clothes, or food.\n\nThis app is completely free and requires no sign-up. It’s designed for sharing items that are **left outside** (on the street or in a publicly accessible location). If the item isn’t directly visible, further pickup instructions may be in the post description. \n\nTo **offer something**, tap the + button, fill in the details, and choose the location.\nTo **pick something up**, go to the posted location and tap the green “Pick up” button.\n\nColor code of pins:\nBlue: Permanent posts (e.g. freedges)\nGreen: Recently posted\nRed: A few days old\n\n\nThis is Free Stuff v\(currentVersion ?? "").\n©Nina Wiedemann (2025)"
     }
 }
 
